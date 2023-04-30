1aba43f156c222427fe9dbfc3d5b57a2c765a99c
Ranjeet Saw <sawranjeet550@gmail.com>
2023-04-30 18:42:25 +0530

    added resume scannerfeature v1

--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
 
 # Create your views here.
 
@@ -10,13 +12,18 @@ def hello(request):
 def resumeScanner(request):
 
     email = request.POST["email"]
-    jd = request.POST["jd"]
+    job_description = request.POST["jd"]
     resume = request.POST["resume"]
 
     # print(email)
     # print(jd)
     # print(resume)
 
-    result = "54%"
+    content_list = [job_description,resume]
+    cv = CountVectorizer()
+    count_matrix = cv.fit_transform(content_list)
+
+    matrix = cosine_similarity(count_matrix)
+    result = (matrix[1][0]*100).round(2)
     
     return render(request, 'index.html', {"result":result})

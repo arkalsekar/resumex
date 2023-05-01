e9b7ad2e51cbab0469909f418d77a165bb5d7e74
Ranjeet Saw <sawranjeet550@gmail.com>
2023-05-01 15:34:52 +0530

    added file upload

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,7 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.hello, name="hello"),
+    path('', views.home, name="home"),
+    path('upload', views.upload, name="upload"),
     path('data', views.resumeScanner, name="resumeScanner"),
 ]
 
